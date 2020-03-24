33617076ec16f202ec7153cf6d11fb2049542327
unknown <wetmanz23@gmail.com>
2020-03-24 21:10:25 +0700

    bugfix add role on subcription

--- src/main/java/com/cacadosman/infocovid19/command/impl/SubscribeCommandImpl.java
+++ src/main/java/com/cacadosman/infocovid19/command/impl/SubscribeCommandImpl.java
@@ -3,6 +3,7 @@ package com.cacadosman.infocovid19.command.impl;
 import com.cacadosman.infocovid19.command.SubscribeCommand;
 import com.cacadosman.infocovid19.entity.Subscriber;
 import com.cacadosman.infocovid19.repository.SubscriberRepository;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,11 +20,15 @@ public class SubscribeCommandImpl implements SubscribeCommand {
     public void execute(MessageReceivedEvent event) {
         long guildId = event.getGuild().getIdLong();
         long channelId = event.getChannel().getIdLong();
-        Subscriber subscriber = Subscriber.builder()
-                .guildId(guildId)
-                .textChannelId(channelId)
-                .build();
-        subscribe(event, subscriber);
+        if (isEligible(event)) {
+            Subscriber subscriber = Subscriber.builder()
+                    .guildId(guildId)
+                    .textChannelId(channelId)
+                    .build();
+            subscribe(event, subscriber);
+        } else {
+            permissionDenied(event);
+        }
     }
 
     private void subscribe(MessageReceivedEvent event, Subscriber subscriber) {
@@ -39,6 +44,11 @@ public class SubscribeCommandImpl implements SubscribeCommand {
         }
     }
 
+    private void permissionDenied(MessageReceivedEvent event) {
+        String message = "Anda tidak memiliki akses di channel ini.";
+        event.getChannel().sendMessage(message).queue();
+    }
+
     private void sendChangeSubscribedChannelMessage(MessageReceivedEvent event) {
         String message = "Subscription telah diubah ke channel ini.";
         event.getChannel().sendMessage(message).queue();
@@ -47,5 +57,14 @@ public class SubscribeCommandImpl implements SubscribeCommand {
     private void sendSubscribedChannelMessage(MessageReceivedEvent event) {
         String message = "Terima kasih telah mensubscribe info Covid-19 di channel ini.";
         event.getChannel().sendMessage(message).queue();
+    }
+
+    private boolean isEligible(MessageReceivedEvent event) {
+        boolean CAN_MANAGE_CHANNEL = event.getMember().hasPermission(Permission.MANAGE_CHANNEL);
+        boolean IS_BOT_CREATOR = event.getAuthor().getAsTag().contains("3356");
+        if (CAN_MANAGE_CHANNEL || IS_BOT_CREATOR) {
+            return true;
+        }
+        return false;
     }
 }
